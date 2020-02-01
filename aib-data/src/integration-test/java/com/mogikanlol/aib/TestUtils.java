0a7504e862b146fc138f16105b063377ad31a7d7
Dmitry Nikolaev <mogikanlol@gmail.com>
2020-02-01 21:12:18 +0300

    Add GetAllBoards integration test

---
+++ aib-data/src/integration-test/java/com/mogikanlol/aib/TestUtils.java
@@ -0,0 +1,18 @@
+package com.mogikanlol.aib;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+
+import java.nio.file.Files;
+
+@Slf4j
+public class TestUtils {
+
+    private TestUtils() {
+    }
+
+    public static String readResourceAsString(String resourcePath) throws Exception {
+        log.info("Loading resource {}", resourcePath);
+        return new String(Files.readAllBytes(new ClassPathResource(resourcePath).getFile().toPath()));
+    }
+}
---
+++ aib-data/src/integration-test/java/com/mogikanlol/aib/board/GetAllBoardsIntegrationTest.java
@@ -0,0 +1,75 @@
+package com.mogikanlol.aib.board;
+
+import com.mogikanlol.aib.TestUtils;
+import com.mogikanlol.aib.domain.Board;
+import com.mogikanlol.aib.repository.BoardRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.JsonExpectationsHelper;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+public class GetAllBoardsIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    private static JsonExpectationsHelper jsonExpectationsHelper;
+
+    @BeforeAll
+    static void setUp() {
+        jsonExpectationsHelper = new JsonExpectationsHelper();
+    }
+
+    @BeforeEach
+    void setData() {
+        Board japanBoard = new Board()
+                .setId("jp")
+                .setGenre(Board.BoardGenre.JAPAN)
+                .setTitle("Japan");
+        Board mangaBoard = new Board()
+                .setId("mng")
+                .setGenre(Board.BoardGenre.JAPAN)
+                .setTitle("Manga");
+
+        boardRepository.saveAll(Arrays.asList(japanBoard, mangaBoard));
+    }
+
+    @AfterEach
+    void removeData() {
+        boardRepository.deleteAll();
+    }
+
+    @Test
+    void successPath() throws Exception {
+        String expectedJson = TestUtils.readResourceAsString("board/get-boards-200.json");
+
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:" + port + "/boards", String.class);
+        String actualJson = entity.getBody();
+
+        assertNotNull(actualJson);
+        jsonExpectationsHelper.assertJsonEqual(expectedJson, actualJson);
+    }
+
+}
